4362244f49c4b225583a83296bfee93eacb2c4ce
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2025-11-13 22:07:52 +0500

    New Graphs Modules added

--- lib/Admin/Dashboard/NumberTypeWidget.dart
+++ lib/Admin/Dashboard/NumberTypeWidget.dart
@@ -4,6 +4,8 @@ import 'package:super_adminPanel/Resources/Colors.dart';
 class NumberTypesWidget extends StatelessWidget {
   const NumberTypesWidget({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
